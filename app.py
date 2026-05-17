b603836104508b78b3de0a734c30145205f5d577
tanvipatel-16 <tanvipatel09112007@gmail.com>
2026-05-17 15:59:55 +0530

    Add new routes and enhance chat API functionality
    Added routes for landing, moods, and chat pages, and improved chat API response formatting.

--- app.py
+++ app.py
@@ -4,25 +4,34 @@ import os
 
 app = Flask(__name__)
 
+# GROQ API
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 client = Groq(
     api_key=GROQ_API_KEY
 )
+
+# PAGE 1
+
 @app.route("/")
 def landing():
     return render_template("landing.html")
+
+# PAGE 2
 
 @app.route("/moods")
 def moods():
     return render_template("index.html")
 
+# PAGE 3
+
 @app.route("/chatpage")
 def chatpage():
     return render_template("chat.html")
 
+# CHAT API
 
-# AI CHAT API
 @app.route("/chat", methods=["POST"])
 def chat():
 
@@ -42,7 +51,18 @@ def chat():
 
                 {
                     "role": "system",
-                    "content": f"You are CookedGPT. Reply in {mood} style."
+                    "content": f"""
+                    You are CookedGPT.
+
+                    Your personality mood is:
+                    {mood}
+
+                    Be funny, emotional,
+                    chaotic, savage,
+                    dramatic and entertaining.
+
+                    Keep replies short-medium.
+                    """
                 },
 
                 {
@@ -60,11 +80,13 @@ def chat():
             "reply": reply
         })
 
-      except Exception as e:
+    except Exception as e:
 
         return jsonify({
             "reply": f"Error: {str(e)}"
         })
-    
+
+# RUN
+
 if __name__ == "__main__":
     app.run(debug=True)
